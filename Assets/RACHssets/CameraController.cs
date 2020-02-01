07ebb7a001ee590ffe69b20d84ccd3283b869395
Stefano Sansone <stefano.sansone@fanduel.com>
2020-02-01 12:00:11 -0500

    import rach player

---
+++ Assets/RACHssets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;        //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    private float helpCameraFollowFaster = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+
+    void LateUpdate()
+    {
+       this.transform.position = new Vector3(Mathf.Lerp(this.transform.position.x, player.transform.position.x + offset.x, Time.deltaTime * helpCameraFollowFaster),
+                                               Mathf.Lerp(this.transform.position.y, player.transform.position.y + offset.y, Time.deltaTime * helpCameraFollowFaster), this.transform.position.z);
+    }
+
+}
---
+++ Assets/RACHssets/PitonInteraction.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitonInteraction : MonoBehaviour
+{
+
+    float health;
+    bool playerInRangeOfPiton;
+    
+    public float maxHealth = 1000.0f;
+    public float repairRate = 10.0f;
+
+    public Sprite brokenSprite;
+    public Sprite damagedSprite;
+    public Sprite healthySprite;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        float startingHealth = Random.Range(0, maxHealth);
+        Debug.Log("Starting health" + startingHealth);
+        health = startingHealth;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRangeOfPiton && Input.GetKey(KeyCode.Space))
+        {
+            health += repairRate * Time.deltaTime; // time since the last frame.  so essentially repairRate is the repair per second that the key is held down?
+            Debug.Log("Repairing the piton, health is now " + health);
+        }
+
+        if (health >= maxHealth)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = healthySprite;
+        }
+        else if (health / maxHealth >= .33)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = damagedSprite;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = brokenSprite;
+        }
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Player collided trigger");
+        playerInRangeOfPiton = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Player exited trigger");
+        playerInRangeOfPiton = false;
+    }
+}
